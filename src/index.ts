8f1fad120362399386f6bbfe3221b78cd2ea0b14
itz-XD <mi125g8256@gmail.com>
2026-05-31 21:45:38 +0530

    chore: add environment configuration

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import http from "node:http";
 import { createExpressApplication } from "./app/index.js";
+import "dotenv/config";
 
 async function main() {
   try {
     const server = http.createServer(createExpressApplication());
-    const PORT: number = 8080;
+    const PORT = Number(process.env.PORT) || 3000;
 
     server.listen(PORT, () => {
       console.log(`HTTP server is running on PORT ${PORT}`);
