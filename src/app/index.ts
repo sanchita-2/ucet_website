e4ff29b0c473d19602af3aebb7951fc80cc9e6b6
Sanchita-2 <sanchitasinha.1101@gmail.com>
2026-06-09 13:30:17 +0530

    dotenv file added

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,12 +1,20 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import type { Express } from "express";
 import { errorHandler } from "./middlewares/error-handler.js";
 
+import testRoutes from "./routes/auth.routes.js";
+
+
+
+
 export function createExpressApplication(): Express {
   const app = express();
 
-  // Middlewares
-
+  // Middlewares]
+app.use("/get", testRoutes);
   // Routes
   app.get("/", (req, res) => {
     return res.status(200).json({
